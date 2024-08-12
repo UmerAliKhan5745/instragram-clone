87d198036cb247134f35c0e37e156d013733a087
umeralikhan <aumeralikhan@gmail.com>
2024-08-12 12:56:34 +0500

    just started

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+    
+    
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1></>
+   
+  );  
+}
+
+export default App;
