38f282624c6fa4906e620923e950e1ac2fe3ea2e
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-03 03:20:18 +0500

    ok

--- server/src/config/dbs.ts
+++ server/src/config/dbs.ts
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 // Import dotenv and configure it to load environment variables from .env
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-console.log( )
+console.log();
 const connectDB = async () => {
-    try {
-    const conn=  await mongoose.connect(process.env.MONGO_URI as string);
-      console.log(`MongoDB connected successfully  ${conn.connection.host}`);
-    } catch (error) {
-      console.error('MongoDB connection error:', error);
-      process.exit(1);
-    }
-  };
-  
-  connectDB();
+  try {
+    console.log("umer");
+    const conn = await mongoose.connect(process.env.MONGO_URI as string);
+    console.log(`MongoDB connected successfully  ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+connectDB();
